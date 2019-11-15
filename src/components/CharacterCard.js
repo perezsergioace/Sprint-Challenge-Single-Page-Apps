986452c3267c5403d245b02d5bb9d54eb579b96b
perezsergioace <perez.sergio.ace@gmail.com>
2019-11-15 14:07:56 -0500

    MVP, styled the character cards.

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,27 @@
 import React from "react";
+import styled from "styled-components"
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+
+  const Card = styled.div`
+     display: flex;
+     flex-wrap: wrap;
+     flex-direction: column;
+     justify-content: center;
+     align-items: center;
+     background-color: #00ff07;
+     margin: 5% 30%
+     border: solid 4px black;
+     border-radius: 40px;
+     `
+
+  return (
+    <Card>
+      <h1>Name: {props.name}</h1>
+      <img src={props.image} />
+      <p>Species: {props.species}</p>
+      <p>Gender: {props.gender}</p>
+      <p>Status: {props.status}</p>
+    </Card>
+  )
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,53 @@
 import React, { useEffect, useState } from "react";
 
+import axios from "axios";
+import { Link } from "react-router-dom";
+import CharacterCard from "./CharacterCard";
+import styled from "styled-components"
+
 export default function CharacterList() {
+
+  const Title = styled.div`
+     display: flex;
+     flex-wrap: wrap;
+     flex-direction: column;
+     justify-content: center;
+     align-items: center;
+     font-size: 50px;
+     `
+
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios.get("https://rickandmortyapi.com/api/character/")
+    .then((response) => {
+      console.log(response.data.results);
+      setCharacters(response.data.results)
+    })
+    .catch((error) => {
+      console.error("Data not returned", error)
+    })
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+    <Link to="/">Home</Link>
+    <Link to="/Search"><button>Search</button></Link>
+
+      <Title>Character List</Title>
+      {characters.map(character => (
+        <CharacterCard 
+        key={character.id} 
+        name={character.name} 
+        image={character.image}
+        species={character.species} 
+        gender={character.gender} 
+        status={character.status}
+        />
+      ))}
     </section>
   );
 }
